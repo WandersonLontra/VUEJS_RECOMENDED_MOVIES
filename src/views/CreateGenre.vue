4bc7ab422d1988056a7c7eaf3a78c7e0a7abf71c
WandersonLontra <wandersonlontra@yahoo.com.br>
2022-01-24 20:26:14 -0300

    Criados as páginas de crição de Movies e Genres

---
+++ src/views/CreateGenre.vue
@@ -0,0 +1,105 @@
+<template>
+   <v-container  class="mt-9">
+       <v-snackbar
+            v-model="snackbar"
+            absolute
+            top
+            :color="snackbarColor"
+            dark
+        >
+            {{ snackbarText }}
+            <v-icon style="left: 10px">
+                {{ snackbarIcon }}
+            </v-icon>
+        </v-snackbar>
+        <v-form ref="createGenreForm" @submit="submitGenres" class="">
+            <h1>Create Genre</h1>
+            <v-text-field 
+                outlined
+                v-model="name"
+                label="Name"
+                required
+            />
+
+            <v-btn
+                block
+                large
+                elevation="3"
+                color="primary"
+                type="submit"
+                :loading="$apollo.loading"
+            >
+                Enviar    
+            </v-btn>            
+        </v-form>
+   </v-container>
+</template>
+<script>
+    import { v4 as uuidv4 } from 'uuid';
+
+
+    const UUID = uuidv4();
+
+    UUID
+
+    export default {
+       data(){
+           return{
+               name: '',
+               genresList: [''],
+               snackbarColor: '',
+               snackbarText: '',
+               snackbarIcon: '',
+               snackbar: false
+           }
+       },
+       apollo: {
+           genres: require('../graphql/Genres.query.gql')
+       },
+       watch: {
+           genres(){
+                this.genresList = this.genres.reduce((acc,sum) => {
+                    acc.push(sum.name);
+                    return acc
+                },[])
+           }
+       },
+       methods: {
+           async submitGenres(event){
+                event.preventDefault();
+
+                const capitalizedName = this.name[0].toUpperCase() + this.name.slice(1);
+                const isGenreExist = this.genresList.includes(capitalizedName);
+
+                if(!isGenreExist){
+                    const result = await this.$apollo.mutate({
+                        mutation: require('../graphql/CreateGenre.mutation.gql'),
+                        variables: {
+                           input: [{
+                               name: capitalizedName
+                           }] 
+                        }
+                    });
+
+
+                    this.snackbarColor = 'success';
+                    this.snackbarText = 'Cadastrado com sucesso!';
+                    this.snackbarIcon = 'mdi-checkbox-marked-circle';
+                    this.snackbar = result.data.createGenres.genres[0].name === this.name;
+
+                    this.genresList.push(capitalizedName)
+                    console.log(this.genresList)
+
+                    if(this.snackbar) this.$refs.createGenreForm.reset();
+
+                } else {
+                    this.snackbarColor = 'warning';
+                    this.snackbarText = 'Este gênero já está listado!';
+                    this.snackbarIcon = 'mdi-alert-circle';
+                    this.snackbar = true;
+                }
+
+           }
+       }
+    }
+</script>
---
+++ src/views/CreateMovie.vue
@@ -0,0 +1,142 @@
+<template>
+   <v-container  class="mt-9">
+       <v-snackbar
+            v-model="snackbar"
+            absolute
+            top
+            color="success"
+            dark
+        >
+            Cadastrado com sucesso!
+            <v-icon style="left: 10px">
+                mdi-checkbox-marked-circle
+            </v-icon>
+        </v-snackbar>
+        <v-form ref="createMovieForm" @submit="submitMovies">
+            <h1>Create Movie</h1>
+            <v-text-field 
+                outlined
+                v-model="title"
+                label="Title"
+                required
+            />
+            <v-text-field 
+                outlined
+                v-model="year"
+                type="number"
+                label="Year"
+                placeholder="2022"
+                required
+            />
+             <v-text-field 
+                outlined
+                v-model="imdbRating"
+                type="number"
+                label="imdbRating"
+                placeholder="8.9"
+                required
+            />
+             <v-text-field 
+                outlined
+                v-model="imageUrl"
+                label="Image URL"
+                placeholder="http://"
+                required
+            />
+            <v-select 
+                outlined
+                v-model="genresSelected"
+                :items="genresList"
+                label="Genres Selection"
+                chips
+                multiple
+                persistent-hint
+            />
+            <v-textarea 
+                outlined
+                label="Movie Plot"
+                v-model="plot"
+            />
+
+            <v-btn
+                block
+                large
+                elevation="3"
+                color="primary"
+                type="submit"
+                :loading="$apollo.loading"
+            >
+                Enviar    
+            </v-btn>            
+        </v-form>
+   </v-container>
+</template>
+<script>
+    import { v4 as uuidv4 } from 'uuid';
+
+    const UUID = uuidv4();
+
+    export default {
+       data(){
+           return{
+               title: '',
+               year: null,
+               imdbRating: null,
+               imageUrl: '',
+               plot: '',
+               genresSelected:[''],
+               genresList: null,
+               snackbar: false
+           }
+       },
+       apollo: {
+           genres: require('../graphql/Genres.query.gql')
+       },
+       watch: {
+           genres(){
+                this.genresList = this.genres.reduce((acc,sum) => {
+                    acc.push(sum.name);
+                    return acc
+                },[])
+           }
+       },
+       methods: {
+           async submitMovies(event){
+                event.preventDefault();
+
+                const genresListConfigured = this.genresSelected.filter(el => el != '').reduce((acc,sum) => {
+                    acc.push({name: sum})
+                    return acc
+                },[])
+
+                const result = await this.$apollo.mutate({
+                    mutation: require('../graphql/CreateMovie.mutation.gql'),
+                    variables: {
+                       input: [{
+                           _id: UUID,
+                           title: this.title,
+                           year: parseInt(this.year),
+                           imdbRating: parseFloat(this.imdbRating),
+                           imageUrl: this.imageUrl,
+                           plot: this.plot,
+                           genres: {
+                               connect: [{
+                                   where: {
+                                       node:{
+                                           OR: genresListConfigured
+                                       }
+                                   }
+                               }]
+                           }
+                       }] 
+                    }
+                })
+                
+                this.snackbar = result.data.createMovies.movies[0].title === this.title
+
+                if(this.snackbar) this.$refs.createMovieForm.reset()
+
+           }
+       }
+    }
+</script>
